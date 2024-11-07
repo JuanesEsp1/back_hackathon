f2dc2c75b93a950a6d2c709d52c68a5f13938988
Sebinhas <jsebascuervo05@gmail.com>
2024-11-07 14:16:56 -0500

    m

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-// ENDPOINTS USUARIOS
+// ############## ENDPOINTS USUARIOS ##############
 app.get("/usuarios", async (req, res) => {
   try {
     const result = "SELECT * FROM users";
@@ -66,7 +66,8 @@ app.delete("/usuarios/:id", async (req, res) => {
   }
 });
 
-// ENDPOINTS PQR
+// ############## ENDPOINTS PQR ##############
+
 app.get("/pqr", async (req, res) => {
   try {
     const result = "SELECT * FROM PQR";
